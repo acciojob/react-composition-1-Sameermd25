aea58eb6a1a889bd3b3d88bfe71b6530b1d310f7
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2026-01-20 16:28:10 +0530

    Add tabsData array for tab content

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,19 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const tabsData = [
+    { title: "Tab 1", content: "This is content for Tab 1" },
+    { title: "Tab 2", content: "This is content for Tab 2" },
+    { title: "Tab 3", content: "This is content for Tab 3" },
+  ];
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Tabs Example</h2>
+      <Tabs tabs={tabsData} />
     </div>
-  )
+  );
+    
 }
 
 export default App
